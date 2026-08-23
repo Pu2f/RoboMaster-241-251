bfc00883a2fcdb5d921ed8510498b9d51557b26c
Pu2f <chatchanok.nt@gmail.com>
2026-08-23 20:28:12 +0700

    Refactor code structure for improved readability and maintainability

--- Assignment_01/14_sensor/01_get_data.py
+++ Assignment_01/14_sensor/01_get_data.py
@@ -19,17 +19,17 @@ from robomaster import robot
 
 if __name__ == '__main__':
     ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="sta")
+    ep_robot.initialize(conn_type="ap")
 
     ep_sensor_adaptor = ep_robot.sensor_adaptor
 
     # 获取传感器转接板adc值
-    adc = ep_sensor_adaptor.get_adc(id=1, port=1)
-    print("sensor adapter id1-port1 adc is {0}".format(adc))
+    adc = ep_sensor_adaptor.get_adc(id=1, port=2)
+    print("sensor adapter id1-port2 adc is {0}".format(adc))
 
     # 获取传感器转接板io电平
-    io = ep_sensor_adaptor.get_io(id=1, port=1)
-    print("sensor adapter id1-port1 io is {0}".format(io))
+    io = ep_sensor_adaptor.get_io(id=4, port=2)
+    print("sensor adapter id4-port2 io is {0}".format(io))
 
     # 获取传感器转接板io电平持续时间
     duration = ep_sensor_adaptor.get_pulse_period(id=1, port=1)
